12735769088dde555a0ff7659bd325e549a4468a
khudeeva <kskhudeeva@gmail.com>
2025-03-12 15:58:21 +0700

    Добавлена таблица 'jobs'. Добавлена функция 'JOIN'. Улучшена структура кода.

--- sqlite_test.py
+++ sqlite_test.py
@@ -15,16 +15,66 @@ CREATE TABLE IF NOT EXISTS users (
 
 print("База данных `test.db` успешно создана и готова к работе!")
 
-#  Удаляем всех пользователей перед вставкой, чтобы избежать дубликатов
+# Создаем таблицу jobs (если её нет)
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS jobs (
+    job_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    job_title TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
+''')
+
+print(f"Таблица 'jobs' успешно создана!")
+
+
+#  Удаляем старые данные перед вставкой, чтобы избежать дубликатов
 cursor.execute("DELETE FROM users")
+cursor.execute("DELETE FROM jobs")
 conn.commit()
 
 # Добавляем пользователей
-cursor.execute("INSERT INTO users (name, age) VALUES ('Анна', 22)")
-cursor.execute("INSERT INTO users (name, age) VALUES ('Иван', 29)")
-cursor.execute("INSERT INTO users (name, age) VALUES ('Мария', 34)")
+users_data = [
+    ("Анна", 22),
+    ("Иван", 29),
+    ("Мария", 34),
+    ("Алексей", 29),
+    ("Светлана", 22)
+]
 
-conn.commit()  # Сохраняем изменения
+cursor.executemany("INSERT INTO users (name, age) VALUES (?, ?)", users_data)
+conn.commit()  # Сохраняем изменения()
+
+# Получаем ID пользователей
+cursor.execute("SELECT id, name FROM users")
+users = cursor.fetchall()
+
+# Создаем список профессий, зная id пользователя
+jobs_data = [
+    (users[0][0], "Разработчик"),
+    (users[1][0], "Тестировщик"),
+    (users[2][0], "Маркетолог"),
+    (users[3][0], "Дизайнер"),
+    (users[4][0], "Аналитик")
+]
+
+# Добавляем профессиии
+cursor.executemany("INSERT INTO jobs (user_id, job_title) VALUES (?, ?)", jobs_data)
+conn.commit()
+
+print("Данные о профессиях добавлены в 'jobs'!")
+
+# Выполняем объединение таблиц 'users' и 'jobs' с 'JOIN'
+cursor.execute('''
+SELECT users.name, users.age, jobs.job_title
+FROM users
+LEFT JOIN jobs ON users.id = jobs.user_id
+''')
+
+print("Список пользоватлей и их профессий:")
+for name, age, job in cursor.fetchall():
+    job = job if job else "Безработный"
+    print(f"Имя: {name}, Возраст: {age}, Профессия: {job}")
 
 # Выбираем всех пользователей
 cursor.execute("SELECT * FROM users")
